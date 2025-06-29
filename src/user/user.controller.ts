10e5c5cb5921781f83cfa51cef7008cfc249ec4b
Aiomedin Chorobaev <eliothab58@gmail.com>
2025-06-29 20:53:49 +0600

    fix

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { RequestWithAuth } from 'src/auth/auth.types';
+import { RequestWithAuth } from '../auth/auth.types';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) { }
 
     @Get()
+    @UseGuards(AuthGuard)
     async getUser(@Req() req: RequestWithAuth) {
         return this.userService.getUser(req);
     }
