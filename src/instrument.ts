2dbfd9e41ad99387acb7a9d823dcfd4f42cb7ddd
Aiomedin Chorobaev <eliothab58@gmail.com>
2025-09-10 17:39:33 +0600

    fix

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,9 +1,6 @@
 import * as Sentry from '@sentry/nestjs';
 
 Sentry.init({
-  dsn: 'https://b6828dee5b3cd083ff2746406ee13eff@o4507503966158848.ingest.de.sentry.io/4509545170337872',
-
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
+  dsn: 'https://925627151dbe979bb2aea055385c5c2d@o4507503966158848.ingest.de.sentry.io/4509991288176720',
   sendDefaultPii: true,
 });
